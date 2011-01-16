7b05c01ab12b8b9c06ba1965d16ff7c2f8655474
Johannes Koggdal <johannes@koggdal.com>
2011-01-16 16:30:54 +0100

    Added display object image.

---
+++ src/displayobjects/image.js
@@ -0,0 +1,63 @@
+(function(oCanvas, window, document, undefined){
+
+	// Define the class
+	var image = function (settings, thecore) {
+		
+		// Return an object when instantiated
+		return oCanvas.extend({
+			core: thecore,
+			
+			loaded: false,
+			loading: false,
+			firstDrawn: false,
+			
+			// Init method for loading the image resource
+			init: function () {
+				var _this = this,
+					source = (this.image.nodeName === "IMG") ? "htmlImg" : "newImg";
+				this.img = (source === "htmlImg") ? this.image.cloneNode(false) : new Image();
+				this.img.onload = function () {
+					_this.loaded = true;
+					_this.width = this.width;
+					_this.height = this.height;
+				};
+				if (source === "newImg")
+					this.img.src = this.image;
+			},
+			
+			// Method that draws the image to the canvas once it's loaded
+			draw: function () {
+				var canvas = this.core.canvas,
+					_this = this;
+				
+				// If the image has finished loading, go on and draw
+				if (this.loaded) {
+				
+					// Draw the image to the canvas
+					canvas.drawImage(this.img, this.x, this.y, this.width, this.height);
+					
+					// Clear the timer if this is the first time it is drawn
+					if (this.firstDrawn === false) {
+						this.firstDrawn = true;
+						clearTimeout(this.loadtimer);
+					}
+				}
+				
+				// If the image hasn't finished loading, set a timer and try again
+				else {
+					clearTimeout(this.loadtimer);
+					this.loadtimer = setTimeout(function () {
+						_this.draw();
+					}, 100);
+				}
+				
+				return this;
+			}
+			
+		}, settings);
+	};
+	
+	// Register the display object
+	oCanvas.registerDisplayObject("image", image, "init");
+	
+})(oCanvas, window, document);
