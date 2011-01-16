d0609aa6f3f1e1b25842b5d7b64241ec35dd1b36
Johannes Koggdal <johannes@koggdal.com>
2011-01-16 15:32:49 +0100

    Enabled triggering of an init method on instantiation of a new display object.

--- src/displayobject.js
+++ src/displayobject.js
@@ -285,16 +285,30 @@
 	// Second namespace where objects gets placed
 	oCanvas.registerModule("display", { wrapper: true });
 	
+	
+	
 	// Add method to oCanvas to enable display objects to be added
-	oCanvas.registerDisplayObject = function (name, obj) {
+	oCanvas.registerDisplayObject = function (name, obj, init) {
+	
 		// Register the object as a submodule to display
 		oCanvas.registerModule("display."+name, {
+		
 			// Method for getting the core instance
 			setCore: function (thecore) {
+			
 				// Method that core.display.rectangle() will call
 				return function (settings) {
-					// Return a new rectangle object that inherits from displayObject
-					return oCanvas.extend(Object.create(displayObject), new obj(settings, thecore));
+				
+					// Create a new rectangle object that inherits from displayObject
+					var retObj = oCanvas.extend(Object.create(displayObject), new obj(settings, thecore));
+					
+					// Trigger an init method if specified
+					if (init !== undefined) {
+						retObj[init]();
+					}
+					
+					// Return the new object
+					return retObj;
 				};
 			}
 		});
