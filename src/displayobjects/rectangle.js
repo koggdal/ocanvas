cfe96efe3193033a21083baf75305772b07eab14
Johannes Koggdal <johannes@koggdal.com>
2011-03-06 19:27:27 +0100

    Fixed a bug where there was half a pixel space between the stroke and fill when it was rotated.

--- src/displayobjects/rectangle.js
+++ src/displayobjects/rectangle.js
@@ -34,7 +34,7 @@
 					
 					// Set stroke outside the box
 					if (this.strokePosition === "outside") {
-						canvas.strokeRect(x - this.strokeWeight / 2, y - this.strokeWeight / 2, this.width + this.strokeWeight, this.height + this.strokeWeight);
+						canvas.strokeRect(x - this.strokeWeight / 2 + 0.5, y - this.strokeWeight / 2 + 0.5, this.width + this.strokeWeight - 1, this.height + this.strokeWeight - 1);
 					}
 					
 					// Set stroke on the edge of the box (half of the stroke outside, half inside)
