f7e3758c90c0860aa8ca641f67acc9d3b33dfd85
Johannes Koggdal <johannes@koggdal.com>
2011-03-13 00:30:10 +0100

    Removed the reverseObject() utility method, since it was never used.

--- src/utils.js
+++ src/utils.js
@@ -8,38 +8,6 @@
 			return new F();
 		};
 	}
-	
-	// Method for reversing the order of an object's properties
-	oCanvas.reverseObject = function (obj) {
-	
-		// Cancel without errors if the passed variable is not an object
-		if (typeof obj !== "object") {
-			return obj;
-		}
-		
-		// Vars
-		var temp_array = [],
-			new_object = {},
-			p, i, l;
-		
-		// Add all properties of the object to a temporary array
-		for (p in obj) {
-			temp_array.push([p, obj[p]]);
-		}
-		
-		// Arrays can be reversed
-		temp_array.reverse();
-		l = temp_array.length;
-		
-		// Remove and re-add all properties in the original object, but now in reverse order
-		for (i = 0; i < l; i++) {
-			delete obj[temp_array[i][0]];
-			obj[temp_array[i][0]] = temp_array[i][1];
-		}
-		
-		// Return reference to the object
-		return obj;
-	};
 
 	// usage: log('inside coolFunc',this,arguments);
 	// http://paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
