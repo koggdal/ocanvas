e8df799d749681423512250c4c39dee315c4fd7f
Johannes Koggdal <johannes@koggdal.com>
2011-10-08 13:55:55 +0200

    Fixed parent offset for lines (issue #11).

--- src/displayobjects/line.js
+++ src/displayobjects/line.js
@@ -24,13 +24,13 @@
 			
 			// Getters and setters
 			set start (values) {
-				this._.start_x = values.x;
-				this._.start_y = values.y;
+				this._.start_x = values.x + (this.parent !== undefined ? this.parent._.abs_x : 0);
+				this._.start_y = values.y + (this.parent !== undefined ? this.parent._.abs_y : 0);
 				this.setPosition();
 			},
 			set end (values) {
-				this._.end_x = values.x;
-				this._.end_y = values.y;
+				this._.end_x = values.x + (this.parent !== undefined ? this.parent._.abs_x : 0);
+				this._.end_y = values.y + (this.parent !== undefined ? this.parent._.abs_y : 0);
 				this.setPosition();
 			},
 			get start () {
@@ -42,40 +42,50 @@
 			
 			// Overwrite the setters that displayObject provides, to enable start/end coordinates to affect the position
 			set x (value) {
+				var diff, offsetX, objects, l, i;
+
 				// Get delta length
-				var diff = this._.end_x - this._.start_x;
+				diff = this._.end_x - this._.start_x;
+
+				// Get parent offset
+				offsetX = this.parent ? this.parent._.abs_x : 0;
 				
 				// Assign new x positions for the object
 				this._.x = value;
-				this._.abs_x = value + ((this.parent !== undefined) ? this.parent.abs_x : 0);
+				this._.abs_x = value + offsetX;
 				
 				// Assign new x positions for start and end points
-				this._.start_x = value - (diff / 2) + (this._.abs_x - this._.x);
-				this._.end_x = value + (diff / 2) + (this._.abs_x - this._.x);
+				this._.start_x = value - (diff / 2) + offsetX;
+				this._.end_x = value + (diff / 2) + offsetX;
 				
 				// Update children
-				var objects = this.children,
-					l = objects.length, i;
+				objects = this.children;
+				l = objects.length;
 				for (i = 0; i < l; i++) {
 					objects[i]._.abs_x = this.abs_x + objects[i].x;
 					objects[i].x += 0;
 				}
 			},
 			set y (value) {
+				var diff, offsetY, objects, l, i;
+
 				// Get delta length
-				var diff = this._.end_y - this._.start_y;
+				diff = this._.end_y - this._.start_y,
+				
+				// Get parent offset
+				offsetY = this.parent ? this.parent._.abs_y : 0;
 				
 				// Assign new y positions for the object
 				this._.y = value;
-				this._.abs_y = value + ((this.parent !== undefined) ? this.parent.abs_y : 0);
+				this._.abs_y = value + offsetY;
 				
 				// Assign new y positions for start and end points
-				this._.start_y = value - (diff / 2) + (this._.abs_y - this._.y);
-				this._.end_y = value + (diff / 2) + (this._.abs_y - this._.y);
+				this._.start_y = value - (diff / 2) + offsetY;
+				this._.end_y = value + (diff / 2) + offsetY;
 				
 				// Update children
-				var objects = this.children,
-					l = objects.length, i;
+				objects = this.children;
+				l = objects.length;
 				for (i = 0; i < l; i++) {
 					objects[i]._.abs_y = this.abs_y - objects[i].y;
 					objects[i].y += 0;
@@ -125,8 +135,13 @@
 			// Method for setting x/y coordinates (which will set abs_x/abs_y as specified by displayObject)
 			setPosition: function () {
 				if (this.initialized) {
-					this.x = this._.start_x + (this._.end_x - this._.start_x) / 2;
-					this.y = this._.start_y + (this._.end_y - this._.start_y) / 2;
+					var offset = { x: 0, y: 0 };
+					if (this.parent !== undefined) {
+						offset.x = this.parent._.abs_x;
+						offset.y = this.parent._.abs_y;
+					}
+					this.x = this._.start_x - offset.x + (this._.end_x - this._.start_x) / 2;
+					this.y = this._.start_y - offset.y + (this._.end_y - this._.start_y) / 2;
 				}
 			},
 			
