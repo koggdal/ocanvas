534cbe50edb84602d38f11e058615764aa62f01a
Johannes Koggdal <johannes@koggdal.com>
2013-01-19 13:24:35 +0100

    (patch): Small refactor of the internal animate method

--- src/animation.js
+++ src/animation.js
@@ -19,51 +19,15 @@
 			},
 
 			animate: function (obj, args) {
-				args = Array.prototype.slice.call(args);
-				args[1] = args[1] || {};
-
-				var props, options, queue, animation;
-
-				// Check for new syntax
-				if (typeof args[1] === "object") {
-					props = args[0];
-
-					// Use passed in options, with fallbacks
-					options = oCanvas.extend({
-						duration: this.defaults.duration,
-						easing: this.defaults.easing,
-						queue: "default",
-						callback: function () {}
-					}, args[1]);
-
-					// Parse the easing option
-					if (typeof options.easing === "string") {
-						// The cubic-bezier() syntax is now deprecated (though it was never really public)
-						if (~options.easing.indexOf("cubic-bezier")) {
-							options.easing = this.getCustomCubicBezier(options.easing) || this.easing[this.defaults.easing];
-						} else {
-							options.easing = this.easing[options.easing] || this.easing[this.defaults.easing];
-						}
-					} else if (typeof options.easing !== "function") {
-						options.easing = this.easing[this.defaults.easing];
-					}
-
-					// Parse duration
-					if (typeof options.duration === "string") {
-						options.duration = (options.duration in module.durations) ? module.durations[options.duration] : module.durations[module.defaults.duration];
-					}
-
-				// Or parse old deprecated syntax
-				} else {
-					props = args.shift();
-					options = this.getAnimateArguments(args);
-				}
+				var argData = this.parseArguments(args);
+				var props = argData.properties;
+				var options = argData.options;
 
 				// Create queue and get it
-				queue = this.queues.create(obj, options.queue);
+				var queue = this.queues.create(obj, options.queue);
 
 				// Create the animation object
-				animation = {
+				var animation = {
 					obj: obj,
 					properties: props,
 					startValues: {},
@@ -79,6 +43,59 @@
 				// Add the animation to the queue and run queue
 				queue.add(animation);
 				queue.run();
+			},
+
+			parseArguments: function (args) {
+				args = Array.prototype.slice.call(args);
+				args[1] = args[1] || {};
+				var options;
+
+				// Check for new syntax
+				if (typeof args[1] === "object") {
+					props = args[0];
+
+					// Use passed in options, with fallbacks
+					options = oCanvas.extend({
+						duration: this.defaults.duration,
+						easing: this.defaults.easing,
+						queue: "default",
+						callback: function () {}
+					}, args[1]);
+
+					options.easing = this.parseEasingOption(options.easing);
+					options.duration = this.parseDurationOption(options.duration);
+
+				// Or parse old deprecated syntax
+				} else {
+					props = args.shift();
+					options = this.getAnimateArguments(args);
+				}
+
+				return {
+					properties: props,
+					options: options
+				};
+			},
+
+			parseEasingOption: function (easing) {
+				if (typeof easing === "string") {
+					// The cubic-bezier() syntax is now deprecated (though it was never really public)
+					if (~easing.indexOf("cubic-bezier")) {
+						return this.getCustomCubicBezier(easing) || this.easing[this.defaults.easing];
+					} else {
+						return this.easing[easing] || this.easing[this.defaults.easing];
+					}
+				} else if (typeof easing !== "function") {
+					return this.easing[this.defaults.easing];
+				}
+			},
+
+			parseDurationOption: function (duration) {
+				var durations = module.durations;
+				if (typeof duration === "string") {
+					return durations[duration] || durations[module.defaults.duration];
+				}
+				return duration;
 			},
 
 			runAnimation: function (animation, callback) {
