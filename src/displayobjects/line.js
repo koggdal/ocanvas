fc201ab7dae0bef9c84d010c68900af6e1b33942
Johannes Koggdal <johannes@koggdal.com>
2013-02-18 21:56:38 +0100

    (patch) line: Fix length value when set at object creation
    When the length property is set in the options object passed
    in when the object is created, the positions must be
    calculated correctly.

--- src/displayobjects/line.js
+++ src/displayobjects/line.js
@@ -15,7 +15,7 @@
 				start_y: 0,
 				end_x: 0,
 				end_y: 0,
-				x: 200,
+				x: 0,
 				y: 0,
 				abs_x: 0,
 				abs_y: 0
@@ -146,8 +146,8 @@
 				dY = Math.cos(angle) * value;
 				this._.end_x = this._.start_x + dX;
 				this._.end_y = this._.start_y + dY;
-				this.x += 0;
-				this.y += 0;
+
+				this.setPosition();
 			},
 			get length () {
 				var dX, dY, length;
@@ -168,15 +168,13 @@
 			
 			// Method for setting x/y coordinates (which will set abs_x/abs_y as specified by displayObject)
 			setPosition: function () {
-				if (this.initialized) {
-					var offset = { x: 0, y: 0 };
-					if (this.parent && !this.parent.isCore) {
-						offset.x = this.parent._.abs_x;
-						offset.y = this.parent._.abs_y;
-					}
-					this.x = this._.start_x - offset.x + (this._.end_x - this._.start_x) / 2;
-					this.y = this._.start_y - offset.y + (this._.end_y - this._.start_y) / 2;
+				var offset = { x: 0, y: 0 };
+				if (this.parent && !this.parent.isCore) {
+					offset.x = this.parent._.abs_x;
+					offset.y = this.parent._.abs_y;
 				}
+				this.x = this._.start_x - offset.x + (this._.end_x - this._.start_x) / 2;
+				this.y = this._.start_y - offset.y + (this._.end_y - this._.start_y) / 2;
 			},
 			
 			// Method for initializing the dimensions
