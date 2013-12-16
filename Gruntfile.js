069981c5387cf8b33bd79bb1a2cce4738c661f13
Johannes Koggdal <johannes@koggdal.com>
2013-12-16 18:23:22 +0100

    Add QuickStart to Grunt script
    QuickStart will build oCanvas and generate two files:
    ./builds/ocanvas.js       (compiled but readable)
    ./builds/ocanvas.min.js   (compiled and minified)

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,27 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+
+    exec: {
+      'quickstart-dev': {
+        cmd: function() {
+          return 'mkdir -p ./builds && ./node_modules/quickstart/quickstart > ./builds/ocanvas.js';
+        }
+      },
+      'quickstart-prod': {
+        cmd: function() {
+          return 'mkdir -p ./builds && ./node_modules/quickstart/quickstart --compress > ./builds/ocanvas.min.js';
+        }
+      }
+    }
   });
+
+  grunt.loadNpmTasks('grunt-exec');
+
+  grunt.registerTask('default', [
+    'exec:quickstart-dev',
+    'exec:quickstart-prod'
+  ]);
 
 };
