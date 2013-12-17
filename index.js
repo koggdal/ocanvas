70ae417d14bc6c83c4f249891cf9edad453d9e4b
Johannes Koggdal <johannes@koggdal.com>
2013-12-17 17:25:55 +0100

    Update index.js to include all modules
    The modules are included in a structure that matches the folder
    structure of the package. This makes it easy to change project
    code from require statements to object access.
    
    If the window object is available, requiring this file will put
    an 'oCanvas' object on window, and it will have the same structure
    as described above. The file outputted by QuickStart when Grunt
    is run will automatically run this file and make everything
    available on window.oCanvas.

---
+++ index.js
@@ -0,0 +1,52 @@
+/**
+ * @module ocanvas
+ */
+'use strict';
+
+if (typeof window !== 'undefined') {
+  window.oCanvas = exports;
+}
+
+/**
+ * Classes that are not shapes.
+ * @type {Object}
+ * @property {Object} Camera The Camera class.
+ * @property {Object} Canvas The Canvas class.
+ * @property {Object} Collection The Collection class.
+ * @property {Object} EventEmitter The EventEmitter class.
+ * @property {Object} World The World class.
+ */
+exports.classes = {
+  Camera: require('./classes/Camera'),
+  Canvas: require('./classes/Canvas'),
+  Collection: require('./classes/Collection'),
+  EventEmitter: require('./classes/EventEmitter'),
+  World: require('./classes/World')
+};
+
+/**
+ * Shape classes.
+ * @type {Object}
+ * @property {Object} Rectangle The Rectangle shape class.
+ * @property {Object} base Base classes for shapes.
+ */
+exports.shapes = {
+  base: {
+    CanvasObject: require('./shapes/base/CanvasObject'),
+    RectangularCanvasObject: require('./shapes/base/RectangularCanvasObject')
+  },
+  Rectangle: require('./shapes/Rectangle')
+};
+
+/**
+ * Utility modules.
+ * @type {Object}
+ * @property {Object} defineProperties The defineProperties module.
+ * @property {Object} inherit The inherit module.
+ * @property {Object} mixin The mixin module.
+ */
+exports.utils = {
+  defineProperties: require('./utils/defineProperties'),
+  inherit: require('./utils/inherit'),
+  mixin: require('./utils/mixin')
+};
