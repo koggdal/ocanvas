7fa4066a749d0547b02ab954911640c913b79fdd
Johannes Koggdal <johannes@koggdal.com>
2013-12-19 21:42:52 +0100

    Move coverage script to Gruntfile and add more test tasks
    grunt               => Simple test output + builds
    grunt coverage      => Generates coverage.html report
    grunt test          => Full test runner
    grunt test-simple   => Full test runner with simple output
    grunt test-unit     => Only normal unit tests
    grunt test-canvas   => Only canvas unit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,50 @@ module.exports = function(grunt) {
         cmd: function() {
           return 'mkdir -p ./builds && ./node_modules/quickstart/quickstart --compress > ./builds/ocanvas.min.js';
         }
+      },
+      'coverage': {
+        cmd: function() {
+          var mochaTests = [
+            'test/unit',
+            'test/canvas'
+          ];
+          var commands = [
+            'mocha ' + mochaTests.join(' ') + ' --require blanket -R html-cov | sed \'s?\'`pwd`\'/??g\' > coverage.html',
+            'echo "\nOpen coverage.html in your browser to see results for code coverage."'
+          ];
+          return commands.join(' && ');
+        }
+      },
+      'test-unit': {
+        cmd: function() {
+          return 'mocha test/unit -R spec';
+        }
+      },
+      'test-canvas': {
+        cmd: function() {
+          var condition = '[ -e node_modules/canvas ]';
+          var tests = 'mocha test/canvas -R spec';
+          var canvasMessage = 'echo "\nNOTE: node-canvas is not installed, so no tests were run.\n"';
+          return condition + ' && ' + tests + ' || ' + canvasMessage;
+        }
+      },
+      'test': {
+        cmd: function() {
+          var condition = '[ -e node_modules/canvas ]';
+          var all = 'mocha test/unit test/canvas -R spec';
+          var unit = 'mocha test/unit -R spec';
+          var canvasMessage = 'echo "\nNOTE: node-canvas is not installed, so only normal unit tests were run.\n"';
+          return condition + ' && ' + all + ' || (' + unit + ' && ' + canvasMessage + ')';
+        }
+      },
+      'test-simple': {
+        cmd: function() {
+          var condition = '[ -e node_modules/canvas ]';
+          var all = 'mocha test/unit test/canvas';
+          var unit = 'mocha test/unit';
+          var canvasMessage = 'echo "\nNOTE: node-canvas is not installed, so only normal unit tests were run.\n"';
+          return condition + ' && ' + all + ' || (' + unit + ' && ' + canvasMessage + ')';
+        }
       }
     }
   });
@@ -21,8 +65,29 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-exec');
 
   grunt.registerTask('default', [
+    'exec:test-simple',
     'exec:quickstart-dev',
     'exec:quickstart-prod'
+  ]);
+
+  grunt.registerTask('coverage', [
+    'exec:coverage'
+  ]);
+
+  grunt.registerTask('test', [
+    'exec:test'
+  ]);
+
+  grunt.registerTask('test-simple', [
+    'exec:test-simple'
+  ]);
+
+  grunt.registerTask('test-unit', [
+    'exec:test-unit'
+  ]);
+
+  grunt.registerTask('test-canvas', [
+    'exec:test-canvas'
   ]);
 
 };
