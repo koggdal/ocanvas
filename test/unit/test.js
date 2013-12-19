184ca0a482b4043db1764f84a78a006e9e765661
Johannes Koggdal <johannes@koggdal.com>
2013-12-19 12:20:04 +0100

    Move utility unit tests to come before class tests

--- test/unit/test.js
+++ test/unit/test.js
@@ -1,12 +1,12 @@
+describe('Utilities', function() {
+  require('./utils/defineProperties');
+  require('./utils/inherit');
+  require('./utils/mixin');
+});
+
 describe('Classes', function() {
   require('./classes/Canvas');
   require('./classes/EventEmitter');
   require('./classes/Collection');
   require('./classes/World');
-});
-
-describe('Utilities', function() {
-  require('./utils/defineProperties');
-  require('./utils/inherit');
-  require('./utils/mixin');
 });
