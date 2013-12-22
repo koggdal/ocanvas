ea93620f5a76a6547856f17577fd6923aae4b48e
Johannes Koggdal <johannes@koggdal.com>
2013-12-22 12:36:19 +0100

    Move out canvas element test mock to its own module

--- test/unit/classes/Canvas.js
+++ test/unit/classes/Canvas.js
@@ -2,44 +2,35 @@ var expect = require('expect.js');
 var Canvas = require('../../../classes/Canvas');
 var Camera = require('../../../classes/Camera');
 var jsonHelpers = require('../../../utils/json');
-
-// Mocks used because the constructor tries to create/validate a canvas element
-var defaultWidth = 300;
-var defaultHeight = 150;
-global.CanvasRenderingContext2D = function() {};
-global.HTMLCanvasElement = function() {
-  this.width = defaultWidth;
-  this.height = defaultHeight;
-};
-global.HTMLCanvasElement.prototype.constructor = global.HTMLCanvasElement;
-global.HTMLCanvasElement.prototype.getContext = function() {
-  return new global.CanvasRenderingContext2D();
-};
-global.document = {
-  createElement: function(tagName) {
-    return tagName === 'canvas' ? new HTMLCanvasElement() : null;
-  }
-};
-
-var HTMLCanvasElement = global.HTMLCanvasElement;
-var CanvasRenderingContext2D = global.CanvasRenderingContext2D;
+var canvasMock = require('../mocks/canvas2d');
 
 describe('Canvas', function() {
 
+  before(function() {
+    canvasMock.on();
+  });
+
+  after(function() {
+    canvasMock.off();
+  });
+
   describe('Canvas constructor', function() {
 
-    var canvas = new Canvas({name: 'Canvas'});
+    var canvas;
+    before(function() {
+      canvas = new Canvas({name: 'Canvas'});
+    });
 
     it('should set any properties passed in', function() {
       expect(canvas.name).to.equal('Canvas');
     });
 
     it('should set the default value of property `width` to the canvas element width', function() {
-      expect(canvas.width).to.equal(defaultWidth);
+      expect(canvas.width).to.equal(canvas.element.width);
     });
 
     it('should set the default value of property `height` to the canvas element height', function() {
-      expect(canvas.height).to.equal(defaultHeight);
+      expect(canvas.height).to.equal(canvas.element.height);
     });
 
     it('should set the default value of property `background` to \'\' (transparent)', function() {
@@ -240,7 +231,10 @@ describe('Canvas', function() {
 
   describe('#_getViewModeValues()', function() {
 
-    var canvas = new Canvas({width: 300, height: 150});
+    var canvas;
+    before(function() {
+      canvas = new Canvas({width: 300, height: 150});
+    });
 
     it('should handle viewMode \'fit-x\' (for horizontal camera)', function() {
       canvas.viewMode = 'fit-x';
---
+++ test/unit/mocks/canvas2d.js
@@ -0,0 +1,42 @@
+var defaultWidth = 300;
+var defaultHeight = 150;
+
+function CanvasRenderingContext2D() {
+}
+
+function HTMLCanvasElement() {
+  this.width = defaultWidth;
+  this.height = defaultHeight;
+}
+HTMLCanvasElement.prototype.constructor = HTMLCanvasElement;
+HTMLCanvasElement.prototype.getContext = function() {
+  if (!this._context) this._context = new global.CanvasRenderingContext2D();
+  return this._context;
+};
+
+var doc = {
+  createElement: function(tagName) {
+    return tagName === 'canvas' ? new global.HTMLCanvasElement() : null;
+  }
+};
+
+var original = {};
+
+exports.on = function() {
+  original.HTMLCanvasElement = global.HTMLCanvasElement;
+  original.CanvasRenderingContext2D = global.CanvasRenderingContext2D;
+  original.document = global.document;
+
+  global.HTMLCanvasElement = HTMLCanvasElement;
+  global.CanvasRenderingContext2D = CanvasRenderingContext2D;
+  global.document = doc;
+};
+
+exports.off = function() {
+  if (original.HTMLCanvasElement)
+    global.HTMLCanvasElement = original.HTMLCanvasElement;
+  if (original.CanvasRenderingContext2D)
+    global.CanvasRenderingContext2D = original.CanvasRenderingContext2D;
+  if (original.document)
+    global.document = original.document;
+};
