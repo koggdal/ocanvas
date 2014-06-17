c05cc33113d8ac84161f2f10290b4ae2aefbc4e4
Johannes Koggdal <johannes@koggdal.com>
2014-06-18 00:41:41 +0200

    Update version to 2.7.2

--- src/core.js
+++ src/core.js
@@ -4,7 +4,7 @@
 	var oCanvas = {
 	    
 		// Version number of this oCanvas release.
-		version: "2.7.1",
+		version: "2.7.2",
 		
 		// Array containing all canvases created by oCanvas on the current page
 		canvasList: [],
