e92670132a9c757430017e6b788f270a262f181c
Johannes Koggdal <johannes@koggdal.com>
2014-10-12 18:08:37 +0200

    Add index file for textures

---
+++ textures/index.js
@@ -0,0 +1,10 @@
+/**
+ * @module ocanvas/textures
+ *
+ * @property {Function} Texture The Texture class.
+ * @property {Function} ColorTexture The ColorTexture class.
+ */
+'use strict';
+
+exports.Texture = require('./Texture');
+exports.ColorTexture = require('./ColorTexture');
