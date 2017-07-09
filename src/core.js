001e9d348a0fce5427a5a13dc28eac6400e8d4b5
Johannes Koggdal <johannes@koggdal.com>
2017-07-09 13:27:52 +0200

    2.8.10

--- src/core.js
+++ src/core.js
@@ -4,7 +4,7 @@
 	var oCanvas = {
 	    
 		// Version number of this oCanvas release.
-		version: "2.8.9",
+		version: "2.8.10",
 		
 		// Array containing all canvases created by oCanvas on the current page
 		canvasList: [],
